f6df4ff3e3051e46cac3c0919db39c0433422e77
JohannaUha <Uha@Johannas-MacBook-Pro.local>
2017-03-21 16:31:10 +0200

    categy recipe count

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
   has_many :recipes, through: :recipe_categories
 
   def number_of_recipes
-    (Recipe.where category_id:category.id).size
+    (Recipe.where category_id:id).size
   end
 end
